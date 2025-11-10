edee561a90fae1d40b26108482a7298449b03fc7
name <gafarova.tasia@gmail.com>
2025-11-10 09:45:36 +0300

    new function MyTableForm

--- src/components/MyTable/MyTable.jsx
+++ src/components/MyTable/MyTable.jsx
@@ -1,29 +1,45 @@
+import {useState} from "react";
+// import MyTableForm from "@/components/MyTable/MyTableForm.jsx";
+import MyTableForm from "./MyTableForm.jsx";
+
 const products = [
-    {title: 'Cabbage', isFruit: false, id: 1},
-    {title: 'Garlic', isFruit: false, id: 2},
-    {title: 'Apple', isFruit: true, id: 3},
-    {title: 'PineApple', isFruit: true, id: 4},
+    {title: 'Cabbage', price: 100, isFruit: false, id: 1},
+    {title: 'Garlic', price: 200, isFruit: false, id: 2},
+    {title: 'Apple', price: 300, isFruit: true, id: 3},
+    {title: 'PineApple', price: 400, isFruit: true, id: 4},
 ];
 
 export default function MyTable() {
-    const productsList = products.map(product =>
+    const [items, setItems] = useState(products);
+
+    const handleAddProduct = (newProduct) => {
+        //todo: bug with id
+        const maxId = Math.max(...products.map(x => x.id)) + 1;
+        const newProductWithId = {...newProduct, id: maxId};
+        setItems([...items, newProductWithId])
+    }
+
+    const productsList = items.map(product =>
         <tr key={product.id}>
             <td className="border px-4 py-2">{product.id}</td>
             <td className="border px-4 py-2">{product.title}</td>
+            <td className="border px-4 py-2">{product.price}</td>
             <td className="border px-4 py-2"
                 style={{color: product.isFruit ? "blue" : " green"}}
-                >
+            >
                 {product.isFruit ? 'yes' : 'no'}</td>
         </tr>
     )
     return (
         <>
             <h3>Table</h3>
+            <MyTableForm onAddProduct={handleAddProduct}/>
             <table className="border border-collapse table-auto w-full">
                 <thead>
                 <tr>
                     <th className="border px-4 py-2">Id</th>
                     <th className="border px-4 py-2">Title</th>
+                    <th className="border px-4 py-2">Price</th>
                     <th className="border px-4 py-2">IsFruit</th>
                 </tr>
                 </thead>
--- src/components/MyTable/MyTableForm.jsx
+++ src/components/MyTable/MyTableForm.jsx
@@ -1,3 +1,26 @@
-export default function MyTableForm(){
-    return <form action=""></form>
+import {useState} from "react";
+
+//todo:
+export default function MyTableForm({onAddProduct}) {
+    const [title, setTitle] = useState('');
+    const [isFruit, setIsFruit] = useState(false);
+    const [price, setPrice] = useState(0);
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        onAddProduct({title: title, isFruit: isFruit, price: price});
+
+        setTitle('');
+        setIsFruit(false);
+        setPrice(0)
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={title} placeholder="title" onChange={(e) => setTitle(e.target.value)} required />
+            <input type="checkbox" checked={isFruit} onChange={(e) => setIsFruit(e.target.checked)} />
+            <input type="text" value={price}  onChange={(e)=> setPrice(Number(e.target.value))} required/>
+            <button type="submit">submit</button>
+        </form>
+    )
+
 }
